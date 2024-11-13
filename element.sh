02894135ce353e7127afc01d37d1ed613fd69ae4
Timothy Sundem <tim.sundem@gmail.com>
2024-11-13 18:27:38 +0000

    fix: updated script for strings

--- element.sh
+++ element.sh
@@ -3,17 +3,28 @@
 #
 if [[ -z "$1" ]]; then
   echo "Please provide an element as an argument."
-  exit 1
+  exit 0
 fi
 
 ELEMENT_INPUT=$1
 
 query_element() {
   PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
-  ELEMENT_INFO=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius, t.type FROM elements e
-                         JOIN properties p ON e.atomic_number = p.atomic_number
-                         JOIN types t ON p.type_id = t.type_id
-                         WHERE e.atomic_number = '$ELEMENT_INPUT' OR e.symbol = '$ELEMENT_INPUT' OR e.name = '$ELEMENT_INPUT';")
+  # Check if int
+  if [[ "$ELEMENT_INPUT" =~ ^[0-9]+$ ]]; then
+    ELEMENT_INFO=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius, t.type 
+                           FROM elements e
+                           JOIN properties p ON e.atomic_number = p.atomic_number
+                           JOIN types t ON p.type_id = t.type_id
+                           WHERE e.atomic_number = '$ELEMENT_INPUT';")
+  else
+    ELEMENT_INFO=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius, t.type 
+                           FROM elements e
+                           JOIN properties p ON e.atomic_number = p.atomic_number
+                           JOIN types t ON p.type_id = t.type_id
+                           WHERE e.symbol = '$ELEMENT_INPUT' OR e.name = '$ELEMENT_INPUT';")
+  fi
+
   echo "$ELEMENT_INFO"
 }
 
@@ -23,7 +34,6 @@ if [[ -z "$ELEMENT_RESULT" ]]; then
   echo "I could not find that element in the database."
 else
   echo "$ELEMENT_RESULT" | while IFS="|" read atomic_number name symbol atomic_mass melting_point boiling_point type; do
-    echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu."
-    echo "$name has a melting point of $melting_point celsius and a boiling point of $boiling_point celsius."
+    echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu. $name has a melting point of $melting_point celsius and a boiling point of $boiling_point celsius."
   done
 fi
